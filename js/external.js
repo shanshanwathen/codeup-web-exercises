a77e566fc8b98957e39a14b80c5825bbd9dca31a
Shanshan Su <shanshansu36@gmail.com>
2021-02-02 16:00:39 -0600

    feat: add new variables

--- js/external.js
+++ js/external.js
@@ -9,11 +9,12 @@ var favoriteColor = prompt("What's your favorite color?");
 alert(favoriteColor[0].toUpperCase() + favoriteColor.substring(1) + " is my favorite color too.");
 
 //renting movies
-var rentDaysOfM = prompt("How many days are you going to rent The Little Mermaid?");
-var rentDaysOfB = prompt("How many days are you going to rent Brother Bear?");
-var rentDaysOfH = prompt("How many days are you going to rent Hercules?");
+var rentDaysOfM = prompt("How many days have you rented The Little Mermaid?");
+var rentDaysOfB = prompt("How many days have you rented Brother Bear?");
+var rentDaysOfH = prompt("How many days have you rented Hercules?");
 var price = prompt("What's the price for each movie per day?");
-alert(price * (Number(rentDaysOfM) + Number(rentDaysOfB) + Number(rentDaysOfH)));
+var totalOwed = price * (Number(rentDaysOfM) + Number(rentDaysOfB) + Number(rentDaysOfH));
+alert("You owe Movie Rental $" + totalOwed + ".");
 
 //pay from work
 var GooglePay = Number(prompt("How much does Google pay you?"));
@@ -22,7 +23,8 @@ var FacebookPay = Number(prompt("How much does Facebook pay you?"));
 var timeForGoogle = Number(prompt("How many hours did you work for Google?"));
 var timeForAmazon = Number(prompt("How many hours did you work for Amazon?"));
 var timeForFacebook = Number(prompt("How many hours did you work for Facebook?"));
-alert(GooglePay * timeForGoogle + AmazonPay * timeForAmazon + FacebookPay * timeForFacebook);
+var totalPay = GooglePay * timeForGoogle + AmazonPay * timeForAmazon + FacebookPay * timeForFacebook;
+alert("You'll receive $" + totalPay + " this week.");
 
 //student enrollment
 var classNotFull = true, scheduleNoConflict = true;
@@ -35,7 +37,7 @@ var isOfferExpired = confirm("Is the offer expired?");
 var premiumMember = confirm("Are you a premium member?");
 var offerCanApply = (numberOfItems >= 2 || premiumMember === true) && !isOfferExpired === true;
 if(offerCanApply === true){
-    alert("I'll apply the offer for you.");
+    alert("Congrats, I'll apply the offer for you.");
 }else{
     alert("I'm sorry that the product offer cannot be applied.");
 }
