7e511fabc422e7960b9c5ab2e0f05b8c649fb556
Shanshan Su <shanshansu36@gmail.com>
2021-02-02 11:17:05 -0600

    feat: add and practice alert, confirm and prompt

--- js/js-in-html-external.js
+++ js/js-in-html-external.js
@@ -3,3 +3,15 @@ console.log("Hello from an external JS file.");
 
 var name = "Shanshan";
 console.log("Hello, " + name);
+
+
+//alert
+//alert("Welcome to my site!");
+
+//confirm
+var confirmLeave = confirm("Are you sure you want to leave? o.o");
+console.log("confirmLeave " + confirmLeave);
+
+//prompt
+var favouriteColor = prompt("What's your favourite color?");
+console.log("Your favourite color is " + favouriteColor);
