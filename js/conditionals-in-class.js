d842dbd4a4dea205aa89826862de71d609f6754f
Shanshan Su <shanshansu36@gmail.com>
2021-02-05 12:22:51 -0600

    finished if, if/else, if/else is/else statement

--- js/conditionals-in-class.js
+++ js/conditionals-in-class.js
@@ -3,10 +3,49 @@
 
     console.log("It's time for some conditionals!!");
 
-    var myName = "Shan";
+    var myAge = Number(prompt("How old are you?"));
+    var requiredAge = 13;
 
-    if(myName === "Shan"){
-        console.log("Hey my name is Shan, too!!");
-    }
+    // This is an if statement
+    // if (myAge >= requiredAge) {
+    //     alert("Congrats, you can make a social media account!");
+    // }
+
+
+    // This is an if/else statement
+    // if (myAge >= requiredAge) {
+    //     alert("Congrats, you can make a social media account!");
+    // } else {
+    //     alert("You are not old enough to make an account!");
+    // }
+
+    // Ternary operator
+    (myAge >= requiredAge) ? console.log("You can make a social media account!") : console.log("You are not old enough to make an account!");
+
+
+
+    // // This is an if/else if/else statement
+    // var favoriteColor = prompt("What's your favorite color?");
+    // if (favoriteColor === "red") {
+    //     alert("Oops, red if not my favorite color.");
+    // } else if (favoriteColor === "blue") {
+    //     alert("Blue is my favorite color too!!");
+    // } else if (favoriteColor === "purple") {
+    //     alert("Purple is my second favorite color!");
+    // } else {
+    //     alert("We don't have the same favorite color.");
+    // }
+
+
+
+    // // // This is a switch statement
+    // switch(myAge) {
+    //     case myAge >= requiredAge:
+    //         alert("Congrats, you can make a social media account!");
+    //         break;
+    //     case myAge < requiredAge:
+    //         alert("You are not old enough to make an account!");
+    //         break;
+    // }
 
 })();
