ce7f0b8fcde6b7bfefeb5010bdeffd36c9266f07
Shanshan Su <shanshansu36@gmail.com>
2021-02-08 09:27:49 -0600

    feat: finished break and continue exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,7 +4,7 @@
     // Break and continue
     function oddNumber() {
         for (var i = 1; i <= 25; i++) {
-            var numberToSkip = prompt("Please type in a number between 1 and 50 :)");
+            var numberToSkip = Number(prompt("Please type in a number between 1 and 50 :)"));
             if (numberToSkip >= 1 && numberToSkip < 50 && numberToSkip % 2 === 1) {
                 console.log("Number to skip is: " + numberToSkip);
                 break;
@@ -15,13 +15,11 @@
             if (number % 2 === 0) {
                 // number is even
                 // skip the even number and continue with the next iteration
-
+                continue;
             } else if (number === numberToSkip) {
                 console.log("Yikes! Skipping number: " + numberToSkip);
-                continue;
-            } else{
-                console.log("Here is an odd number: " + number);
             }
+            console.log("Here is an odd number: " + number);
         }
     }
 
