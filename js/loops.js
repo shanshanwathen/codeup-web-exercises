c1772222689554b79b74165b122f0b3dd05ed93f
Shanshan Su <shanshansu36@gmail.com>
2021-02-07 18:52:07 -0600

    feat: finished for loop exercises

--- js/loops.js
+++ js/loops.js
@@ -1,7 +1,7 @@
-//(function (){
+(function (){
     "use strict";
-    // For loops
-    //2
+
+    // For loops 2
     function showMultiplicationTable(number) {
         for (var i = 1; i <=10; i++) {
             console.log(number + "x" + i + " = " + number * i);
@@ -10,19 +10,19 @@
 
     showMultiplicationTable(7);
 
-    // //3
-    // function isEvenOrOdd() {
-    //     for (var i = 1; i <= 10; i++) {
-    //         var random = Math.floor(Math.random() * (200 - 20) + 20);
-    //         if (random % 2 === 0) {
-    //             console.log(random + " is even.");
-    //         } else {
-    //             console.log(random + " is odd.");
-    //         }
-    //     }
-    // }
-    //
-    // isEvenOrOdd();
+    //3
+    function isEvenOrOdd() {
+        for (var i = 1; i <= 10; i++) {
+            var random = Math.floor(Math.random() * (200 - 20) + 20);
+            if (random % 2 === 0) {
+                console.log(random + " is even.");
+            } else {
+                console.log(random + " is odd.");
+            }
+        }
+    }
+
+    isEvenOrOdd();
 
     //3
     function evenOrOdd() {
@@ -45,9 +45,20 @@
 
     //4
     function printNumber() {
-        for () {
-
+        for (var i = 1; i < 10; i++) {
+            console.log(i.toString().repeat(i));
         }
     }
 
-//})();
+    printNumber();
+
+    //5
+    function minusFive() {
+        for (var i = 100; i > 0; i -= 5) {
+            console.log(i);
+        }
+    }
+
+    minusFive();
+
+})();
