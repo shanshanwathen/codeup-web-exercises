598577986f5d386c3fc83ed1ba4f8c62c6292147
Shanshan Su <shanshansu36@gmail.com>
2021-02-08 12:06:13 -0600

    feat: fixed the do while loop exercise

--- js/while.js
+++ js/while.js
@@ -17,18 +17,16 @@
         // This is how you get a random number between 50 and 100
         var allCones = Math.floor(Math.random() * 50) + 50;
         console.log("I have " + allCones + " cones to sell before I go home today :)");
-        var conesSold = 0;
+        var conesSold = 0, conesLeft = allCones;
         do {
             var conesToBuy = Math.floor(Math.random() * 5) + 1;
-            conesSold = conesSold + conesToBuy;
-            var conesLeft = allCones - conesSold;
             if (conesLeft >= conesToBuy) {
+                conesSold = conesSold + conesToBuy;
+                conesLeft = conesLeft - conesToBuy;
                 console.log(conesToBuy + " cones sold...");
-                console.log(conesLeft + " cones left.");
             } else if (conesLeft < conesToBuy) {
                 console.log("Cannot sell you " + conesToBuy + " cones. I only have " + conesLeft + "...");
             }
-
         } while (conesLeft > 0);
         console.log("Yay! I sold them all!");
     }
