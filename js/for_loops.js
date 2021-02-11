46e34dc834ca377d7844ae3de2de1e3c5a951548
Shanshan Su <shanshansu36@gmail.com>
2021-02-11 16:59:36 -0600

    created planets files

--- js/for_loops.js
+++ js/for_loops.js
@@ -16,7 +16,7 @@
     // Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
     function isEvenOrOdd() {
         for (var i = 1; i <= 10; i++) {
-            var random = Math.floor(Math.random() * (200 - 20) + 20);
+            var random = Math.floor(Math.random() * 181 + 20);
             if (random % 2 === 0) {
                 console.log(random + " is even.");
             } else {
