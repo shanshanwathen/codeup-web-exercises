4a6660eea45c2b4445df81f87658f2ba6bb222cc
Shanshan Su <shanshansu36@gmail.com>
2021-02-22 15:46:58 -0600

    simplified the code

--- js/while.js
+++ js/while.js
@@ -3,6 +3,11 @@
 
     //while loops
     function double() {
+        // var number = 2;
+        // while (number <= 65536) {
+        //     console.log(number);
+        //     number *= 2;
+        // }
         var i = 1;
         while (i <=16) {
             console.log(2 ** i);
@@ -17,16 +22,15 @@
         // This is how you get a random number between 50 and 100
         var allCones = Math.floor(Math.random() * 50) + 50;
         console.log("I have " + allCones + " cones to sell before I go home today :)");
-        var conesLeft = allCones;
         do {
-            var conesToBuy = Math.floor(Math.random() * 5) + 1;
-            if (conesLeft >= conesToBuy) {
-                conesLeft = conesLeft - conesToBuy;
-                console.log(conesToBuy + " cones sold...");
+            var conesToSell = Math.floor(Math.random() * 5) + 1;
+            if (allCones >= conesToSell) {
+                allCones = allCones - conesToSell;
+                console.log(conesToSell + " cones sold...");
             } else {
-                console.log("Cannot sell you " + conesToBuy + " cones. I only have " + conesLeft + "...");
+                console.log("Cannot sell you " + conesToSell + " cones. I only have " + allCones + "...");
             }
-        } while (conesLeft > 0);
+        } while (allCones > 0);
         console.log("Yay! I sold them all!");
     }
 
