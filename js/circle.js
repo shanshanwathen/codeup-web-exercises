9512e1bf61f4061079499ab89139bb719d0f2db8
Shanshan Su <shanshansu36@gmail.com>
2021-02-22 11:36:05 -0600

    finished exercise

--- js/circle.js
+++ js/circle.js
@@ -8,17 +8,21 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            var area = Math.PI * circle.radius * circle.radius;
+            return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            if (doRounding === true) {
+                doRounding = Math.round(circle.getArea());
+            } else {
+                doRounding = circle.getArea();
+            }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + doRounding);
         }
     };
 
@@ -30,7 +34,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
