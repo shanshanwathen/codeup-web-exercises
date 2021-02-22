dd99a1499538ac5cbac4209ecc04d8f7c7eb6689
Shanshan Su <shanshansu36@gmail.com>
2021-02-22 12:04:10 -0600

    refactored isEvenOrOdd function

--- js/for_loops.js
+++ js/for_loops.js
@@ -14,15 +14,25 @@
 
     //3
     // Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+    // function isEvenOrOdd() {
+    //     for (var i = 1; i <= 10; i++) {
+    //         var random = Math.floor(Math.random() * 181 + 20);
+    //         if (random % 2 === 0) {
+    //             console.log(random + " is even.");
+    //         } else {
+    //             console.log(random + " is odd.");
+    //         }
+    //     }
+    // }
+
     function isEvenOrOdd() {
+        var randomNumbers = [];
         for (var i = 1; i <= 10; i++) {
-            var random = Math.floor(Math.random() * 181 + 20);
-            if (random % 2 === 0) {
-                console.log(random + " is even.");
-            } else {
-                console.log(random + " is odd.");
-            }
+            randomNumbers.push(Math.floor(Math.random() * 181 + 20));
         }
+        return randomNumbers.every(function(element) {
+            (element % 2 === 0) ? console.log(element + "is even.") : console.log(element + " is odd.");
+        });
     }
 
     isEvenOrOdd();
@@ -31,7 +41,7 @@
     function evenOrOdd() {
         var i = 1;
         while (i <= 10) {
-            var random = Math.floor(Math.random() * (200 - 20) + 20);
+            var random = Math.floor(Math.random() * (200 - 20 + 1) + 20);
             switch (random % 2) {
                 case 0:
                     console.log(random + " is even.");
