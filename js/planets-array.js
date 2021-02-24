da1989eae327f9425f31c03499be984bc091b12e
Shanshan Su <shanshansu36@gmail.com>
2021-02-24 12:06:49 -0600

    changed order

--- js/planets-array.js
+++ js/planets-array.js
@@ -18,20 +18,23 @@
      * the step that it describes
      */
 
-    planets.unshift("The Sun");
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift("The Sun");
     console.log(planets);
 
-    planets.push("Pluto");
+
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
     console.log(planets);
 
-    planets.shift();
+
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
     console.log(planets);
 
-    planets.pop();
+
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
