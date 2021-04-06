b4dd8b56eceb5c921ba7c698f53c1471b154eda9
Shanshan Su <shanshansu36@gmail.com>
2021-04-06 14:02:23 -0500

    feat: finished jQuery selectors exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-    alert("Your file is done loading and ready to go!");
+    //
+    // // ID Selectors
+    // var summary = $('#summary').html();
+    // alert(summary);
+    //
+    // var title = $('#title').html();
+    // alert(title);
+    //
+    // // Class Selectors
+    // $('.codeup').css('border','1px solid red');
+    //
+    // Element Selectors
+    $('li').css({'font-size': '20px', 'background': 'yellow'});
+    $('h1').css('background', 'yellow');
+    $('p').css('background', 'yellow');
+    alert($('h1').html());
+
+    // Multiple Selectors
+    $('h1, p, li').css('background', 'yellow');
+
+
+
+    // this ones neat, courtesy of Victor
+    // $("*").click(function(){
+    //     $('*').hide()
+    // })
+    // $('*').dblclick(function(){
+    //     $('*').show()
+    // })
+
 });
