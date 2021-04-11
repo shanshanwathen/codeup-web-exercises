2194b9162eba34c41fdb3c25c5335ee6cda63209
Shanshan Su <shanshansu36@gmail.com>
2021-04-10 21:33:50 -0500

    feat: completed mapbox API exercises

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -2,23 +2,35 @@
 
 mapboxgl.accessToken = mapboxToken;
 
-geocode("9030 Wurzbach Rd, San Antonio, TX 78240", mapboxToken).then(function(results1) {
-    var map = new mapboxgl.Map({
-     container: "map", // container ID
-     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-     center: results1, // starting position [lng, lat]
-     zoom: 9 // starting zoom
-    });
+var myFavoriteRestaurants = [{
+        name: "Fujiya Japanese Garden",
+        address: "9030 Wurzbach Rd, San Antonio, TX 78240"
+    }, {
+        name: "Sushihana Japanese Restaurant",
+        address: "1810 NW Military Hwy, San Antonio, TX 78213"
+    }, {
+        name: "Hon Machi Korean BBQ",
+        address: "8251 Agora Pkwy, Selma, TX 78154"
+}]
 
-    var fujiyapPopup = new mapboxgl.Popup()
-        .setHTML("<h6>Fujiya Japanese Garden</h6>");
-
-    var fujiyaMarker = new mapboxgl.Marker({color: "orange"})
-        .setLngLat(results1)
-        .setPopup(fujiyapPopup)
-        .addTo(map);
-
+var map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-98.4951, 29.4246], // starting position [lng, lat]
+    zoom: 9 // starting zoom
 });
+
+
+myFavoriteRestaurants.forEach(function(restaurant) {
+    geocode(restaurant.address, mapboxToken).then(function(results) {
+        new mapboxgl.Marker({color: "orange"})
+            .setLngLat(results)
+            .setPopup(new mapboxgl.Popup()
+            .setHTML("<h6>" + restaurant.name + "</h6>"))
+            .addTo(map);
+    });
+});
+
 
 
 
