ecb076f2c1168cbe9711f28fb5126d00fad7de83
Shanshan Su <shanshansu36@gmail.com>
2021-04-12 18:41:56 -0500

    feat: finished the function to display 5 day weather data, need to work ong the icons

--- js/weather_map.js
+++ js/weather_map.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             .addTo(map);
     });
 
-    var weathersIn5Days = [];
+    var weatherIn5Days = [];
 
     // GET REQUEST to OpenWeatherMap using city parameter
     // 5 day forecast info
@@ -21,18 +21,31 @@ $(document).ready(function () {
         appid: openWeatherAppId,
         units: "imperial"
     }).done(function (results) {
-        var weathersInEvery3Hrs = results.list;   // 5 day / 3 hour forecast data
+        var weathersInEvery3Hrs = results.list;   // 5 day / 3 hour forecast data (0:00am, 3:00, 6:00, 9:00, 12:00pm, 3:00pm, 6:00pm, 9:00pm)
 
         // filter out the weathers at 9:00am everyday
         weathersInEvery3Hrs.forEach(function (weather) {
             if (weather.dt_txt.split(" ")[1] === "09:00:00") {
-                weathersIn5Days.push(weather);
+                weatherIn5Days.push(weather);
             }
         });
-        console.log(weathersIn5Days);
+        console.log(weatherIn5Days);
+
+        for (var i = 0; i < weatherIn5Days.length; i++) {
+            var html = '<h5 class="card-title bg-light">' + weatherIn5Days[i].dt_txt.split(" ")[0] + '</h5>';
+            html += '<h6 class="temperature">' + weatherIn5Days[i].main.temp_max + '/' + weatherIn5Days[i].main.temp_min + '</h6>';
+            html += '<h6>' + weatherIn5Days[i].weather[0].icon + '</h6>';
+            html += '<div class="card-body">';
+            html += '<ul class="card-text list-group">';
+            html += '<li class="list-group-item">Description: ' + weatherIn5Days[i].weather[0].description + '</li>';
+            html += '<li class="list-group-item">Humidity: ' + weatherIn5Days[i].main.humidity + '</li>';
+            html += '<li class="list-group-item">Wind: ' + weatherIn5Days[i].wind.speed + '</li>';
+            html += '<li class="list-group-item">Pressure: ' + weatherIn5Days[i].main.pressure + '</li>';
+            html += '</ul></div>';
+            $(".card").eq(i).html(html);
+        }
     });
 
-    console.log(weathersIn5Days);
 
 });
 
