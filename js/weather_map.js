1319352dc2fddfe8dfd36237367aa4f8d05bacc1
Shanshan Su <shanshansu36@gmail.com>
2021-04-12 17:04:30 -0500

    feat: added html needed to display weather in 5 days and got function to filter out 5 weather data objects

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,26 +2,37 @@ $(document).ready(function () {
 
     "use strict";
 
+    geocode("san Antonio, TX", mapboxToken).then(function (results) {
+        new mapboxgl.Marker()
+            .setLngLat(results)
+            .addTo(map);
+    });
+
+    var weathersIn5Days = [];
+
+    // GET REQUEST to OpenWeatherMap using city parameter
+    // 5 day forecast info
+    // https://api.openweathermap.org/data/2.5/forecast
 
 
+    // Call 5 day / 3 hour forecast data
+    $.get("https://api.openweathermap.org/data/2.5/forecast",  {
+        q: "San Antonio, US",
+        appid: openWeatherAppId,
+        units: "imperial"
+    }).done(function (results) {
+        var weathersInEvery3Hrs = results.list;   // 5 day / 3 hour forecast data
 
-    // geocode("san Antonio, TX", mapboxToken).then(function (results) {
-    //     new mapboxgl.Marker()
-    // })
-    // // GET REQUEST to OpenWeatherMap using city parameter
-    // // https://openweathermap.org/current
-    //
-    // $.get("https://api.openweathermap.org/data/2.5/weather",  {
-    //     q: "San Antonio, US",
-    //     appid: openWeatherAppId,
-    //
-    //     // TODO: How can we get imperial units instead?
-    //     units: "imperial"
-    // }).done(function (results) {
-    //     console.log(results);
-    // })
+        // filter out the weathers at 9:00am everyday
+        weathersInEvery3Hrs.forEach(function (weather) {
+            if (weather.dt_txt.split(" ")[1] === "09:00:00") {
+                weathersIn5Days.push(weather);
+            }
+        });
+        console.log(weathersIn5Days);
+    });
 
-
+    console.log(weathersIn5Days);
 
 });
 
